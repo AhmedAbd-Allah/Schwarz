200fb1a25e86785f05eaaf6e8f91af1890e4f75b
AhmedAbd-Allah <ahmedabdallah.programming@gmail.com>
2020-04-23 00:57:14 +0200

    create model

---
+++ app/Models/order.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+    orderId: {
+        type: String,
+        unique: true
+    },
+    companyName: {
+        type: String,
+    },
+    customerAdress: {
+        type: String,
+    },
+    orderedItem: {
+        type: String,
+    }
+},
+    { timestamps: true });
+
+
+mongoose.model('Orders', orderSchema);
+module.exports = mongoose.model('Orders');
